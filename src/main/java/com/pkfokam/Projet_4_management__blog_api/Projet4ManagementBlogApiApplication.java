3b3d35a4ee4236c7ac0e7568d0b45cc9dd177ced
DevComique <kpambahypolite1@gmail.com>
2026-06-20 02:32:01 +0100

    initialisation du projet blog_api

---
+++ src/main/java/com/pkfokam/Projet_4_management__blog_api/Projet4ManagementBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.pkfokam.Projet_4_management__blog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet4ManagementBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet4ManagementBlogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pkfokam/Projet_4_management__blog_api/Projet4ManagementBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkfokam.Projet_4_management__blog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet4ManagementBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
